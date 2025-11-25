cc36e12898036cd447d5136b03a2ad878e78ed54
M Bussonnier <bussonniermatthias@gmail.com>
2025-11-26 00:10:45 +0100

    Revert "node if has no () or \n"
    This reverts commit a855ba21fa9f7fc5facefd10b2160101db37ba96.

--- src/pages/status/migration/index.jsx
+++ src/pages/status/migration/index.jsx
@@ -597,8 +597,8 @@ function ImpactTable({ graphDataStructure, details }) {
     // === ASSOCIATE SVG ELEMENTS WITH DATA ===
     // Add data attributes to SVG elements so we can find them later
     svgGroup.selectAll("g.node").each(function () {
-      const fullText = d3.select(this).select("text");
-      const nodeId = fullText;
+      const fullText = d3.select(this).select("text").text().split("\n")[0];
+      const nodeId = fullText.split("(")[0].trim();
       d3.select(this).attr("data-node-id", nodeId);
 
       // Highlight selected node
