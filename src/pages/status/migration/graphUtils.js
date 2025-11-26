fe76c852f8e45d5a9d940f0ed1521984cdb4e967
M Bussonnier <bussonniermatthias@gmail.com>
2025-11-26 06:50:37 +0100

    Revert "undo lenght0 check"
    This reverts commit 76fce92b11c57f00b03b55265333d3e2a0d5ce5d.

--- src/pages/status/migration/graphUtils.js
+++ src/pages/status/migration/graphUtils.js
@@ -211,10 +211,6 @@ export const buildGraphDataStructure = (feedstockStatus) => {
 export const buildInitialGraph = (graphDataStructure) => {
   const { nodeMap, edgeMap, allNodeIds } = graphDataStructure;
 
-  if (!allNodeIds || allNodeIds.length === 0) {
-    return null;
-  }
-
   // Identify nodes that have direct children using nodeMap
   const nodesWithChildren = new Set();
   allNodeIds.forEach(nodeId => {
