972f58e3bdda2635c3ed9192424cb427bd9de8cb
M Bussonnier <bussonniermatthias@gmail.com>
2025-12-11 21:22:24 +0100

    components

--- src/components/DependencyGraph/Legend.jsx
+++ src/components/DependencyGraph/Legend.jsx
@@ -2,49 +2,36 @@ import React from "react";
 import graphStyles from "./graphStyles.module.css";
 
 /**
+ * LegendItem component that displays a single legend entry with a colored circle and label.
+ */
+function LegendItem({ colorClass, label }) {
+  return (
+    <div className={graphStyles.legendItem}>
+      <span className={`${graphStyles.legendCircle} ${colorClass}`}></span>
+      <span className={graphStyles.legendLabel}>{label}</span>
+    </div>
+  );
+}
+
+/**
  * Legend component that displays the color coding for different node statuses
  * in the migration dependency graph.
  */
 export default function Legend() {
+  const items = [
+    { colorClass: graphStyles.legendSuccess, label: "CI Passing" },
+    { colorClass: graphStyles.legendDanger, label: "CI Failing" },
+    { colorClass: graphStyles.legendWarning, label: "Bot/Solver Error or Status Unknown" },
+    { colorClass: graphStyles.legendAwaitingPr, label: "Awaiting PR" },
+    { colorClass: graphStyles.legendAwaitingParents, label: "Awaiting Parents" },
+    { colorClass: graphStyles.legendDashed, label: "Awaiting Parent in Another Migration" },
+  ];
+
   return (
-    <div className={graphStyles.legend}>
-      <div className={graphStyles.legendItems}>
-        {/* Legend item: CI Passing */}
-        <div className={graphStyles.legendItem}>
-          <span className={`${graphStyles.legendCircle} ${graphStyles.legendSuccess}`}></span>
-          <span className={graphStyles.legendLabel}>CI Passing</span>
-        </div>
-
-        {/* Legend item: CI Failing */}
-        <div className={graphStyles.legendItem}>
-          <span className={`${graphStyles.legendCircle} ${graphStyles.legendDanger}`}></span>
-          <span className={graphStyles.legendLabel}>CI Failing</span>
-        </div>
-
-        {/* Legend item: Bot/Solver Error or Status Unknown */}
-        <div className={graphStyles.legendItem}>
-          <span className={`${graphStyles.legendCircle} ${graphStyles.legendWarning}`}></span>
-          <span className={graphStyles.legendLabel}>Bot/Solver Error or Status Unknown</span>
-        </div>
-
-        {/* Legend item: Awaiting PR */}
-        <div className={graphStyles.legendItem}>
-          <span className={`${graphStyles.legendCircle} ${graphStyles.legendAwaitingPr}`}></span>
-          <span className={graphStyles.legendLabel}>Awaiting PR</span>
-        </div>
-
-        {/* Legend item: Awaiting Parents */}
-        <div className={graphStyles.legendItem}>
-          <span className={`${graphStyles.legendCircle} ${graphStyles.legendAwaitingParents}`}></span>
-          <span className={graphStyles.legendLabel}>Awaiting Parents</span>
-        </div>
-
-        {/* Legend item: Awaiting Parent in Another Migration */}
-        <div className={graphStyles.legendItem}>
-          <span className={`${graphStyles.legendCircle} ${graphStyles.legendDashed}`}></span>
-          <span className={graphStyles.legendLabel}>Awaiting Parent in Another Migration</span>
-        </div>
-      </div>
+    <div className={`${graphStyles.legend} ${graphStyles.legendItems}`}>
+      {items.map((item) => (
+        <LegendItem key={item.label} colorClass={item.colorClass} label={item.label} />
+      ))}
     </div>
   );
 }
